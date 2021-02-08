38ed93a386fa8e5280578806298595a4e6b4fda7
Kriangsak1997 <kriangsak.thi@student.mahidol.edu>
2021-02-08 21:19:49 +0700

    a bucnha things added

---
+++ better-todo-appo/src/components/Todo.vue
@@ -0,0 +1,14 @@
+<template>
+<h1>Todo</h1>
+</template>
+
+<script>
+
+export default {
+  name: 'Todo',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
--- better-todo-appo/src/router/index.js
+++ better-todo-appo/src/router/index.js
@@ -1,28 +1,32 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import Login from '../components/Login.vue';
+import Register from '../components/Register.vue';
+import Todo from '../components/Todo.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home,
+    path: '/Login',
+    name: 'Login',
+    alias: '/',
+    component: Login,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/Register',
+    name: 'Register',
+    component: Register,
+  },
+  {
+    path: '/Todo',
+    name: 'Todo',
+    component: Todo,
   },
 ];
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
+  mode: 'hash',
   routes,
 });
 
